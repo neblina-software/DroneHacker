a47d2459d98d12739f13447c9eea4dcf4f152028
Anibal Gomez <anibalgomez@balerocms.com>
2015-03-30 15:47:26 -0600

    MPU6050

--- FlightController/FlightController.ino
+++ FlightController/FlightController.ino
@@ -44,6 +44,31 @@
 
 #include <PinChangeInt.h>
 #include <Servo.h>
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
+
+Quaternion q;
+VectorInt16 aa;
+VectorInt16 aaReal;
+VectorInt16 aaWorld;
+VectorFloat gravity;
+float euler[3];
+float ypr[3];
+
+int yaw, pitch, roll;
 
 #define THROTTLE_IN_PIN 5
 
@@ -65,8 +90,20 @@ volatile uint16_t unThrottleInShared;
 
 uint32_t ulThrottleStart;
 
+volatile bool mpuInterrupt = false;
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
 void setup(){
   
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        TWBR = 24;
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    
   Serial.begin(115200);
   Serial.println("multiChannels");
 
@@ -78,6 +115,26 @@ void setup(){
   PCintPort::attachInterrupt(THROTTLE_IN_PIN, calcThrottle, CHANGE); 
   
   arm();
+  
+  mpu.initialize();
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  devStatus = mpu.dmpInitialize();
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788);
+  
+  if (devStatus == 0) {
+        mpu.setDMPEnabled(true);
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
 
 }
 
@@ -86,6 +143,20 @@ void loop() {
   static uint16_t unThrottleIn;
 
   static uint8_t bUpdateFlags;
+
+  if (!dmpReady) return;
+    while (!mpuInterrupt && fifoCount < packetSize) {
+    }
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        mpu.resetFIFO();
+    } else if (mpuIntStatus & 0x02) {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+    }
 
   if(bUpdateFlagsShared) {
     
@@ -103,7 +174,7 @@ void loop() {
     if(servoMotorTL.readMicroseconds() && servoMotorTR.readMicroseconds() 
         && servoMotorBL.readMicroseconds() && servoMotorBR.readMicroseconds()
         != unThrottleIn) {
-          Serial.println(unThrottleIn);
+          //Serial.println(unThrottleIn);
           stabilized(unThrottleIn);
     }
   }
@@ -127,10 +198,57 @@ void arm() {
   servoMotorBR.writeMicroseconds(1000);
 }
 
-void stabilized(uint16_t unThrottleIn) {
-  servoMotorTL.writeMicroseconds(unThrottleIn);
-  servoMotorTR.writeMicroseconds(unThrottleIn);
-  servoMotorBL.writeMicroseconds(unThrottleIn);
-  servoMotorBR.writeMicroseconds(unThrottleIn);
+void stabilized(int unThrottleIn) {
+  
+  int tl, tr, bl, br;
+  
+  tl = unThrottleIn;
+  tr = unThrottleIn;
+  bl = unThrottleIn;
+  br = unThrottleIn;
+  
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  
+  yaw = ypr[0] * 180/M_PI;
+  pitch = ypr[1] * 180/M_PI;
+  roll = ypr[2] * 180/M_PI;
+  
+  if(unThrottleIn > 1060) {
+  
+  if(pitch > 3) {
+    tl = tl + 100;
+    bl = bl + 100;
+  }
+  
+  if(pitch < -3) {
+    tr = tr + 100;
+    br = br + 100;
+  }
+  
+  if(roll > 3) {
+    bl = bl + 100;
+    br = br + 100;
+  }
+  
+  if(roll < -3) {
+    tl = tl + 100;
+    tr = tr + 100;
+  }
+  
+  }
+  
+  //Serial.println("Valor a motor");
+  //Serial.println(tl);
+  //Serial.println(tr);
+  //Serial.println(bl);
+  //Serial.println(br);
+  
+  servoMotorTL.writeMicroseconds(tl);
+  servoMotorTR.writeMicroseconds(tr);
+  servoMotorBL.writeMicroseconds(bl);
+  servoMotorBR.writeMicroseconds(br);
+  
 }
 
